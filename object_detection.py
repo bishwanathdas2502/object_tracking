25c16f4dd796d08d52f0fddfb60f233d400a84d0
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2019-10-10 23:43:27 +0530

    Add files via upload

---
+++ object_detection.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    return x
+
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow('Tracking')
+# setting lower hue
+cv2.createTrackbar('LH','Tracking',0,255,nothing)
+# setting lower saturation
+cv2.createTrackbar('LS','Tracking',0,255,nothing)
+# setting lower value
+cv2.createTrackbar('LV','Tracking',0,255,nothing)
+
+#setting upper hue
+cv2.createTrackbar('UH','Tracking',255,255,nothing)
+#setting upper saturation
+cv2.createTrackbar('US','Tracking',255,255,nothing)
+# setting upper value
+cv2.createTrackbar('UV','Tracking',255,255,nothing)
+
+
+
+
+while 1:
+
+    _,frame = cap.read()
+
+
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    l_h = cv2.getTrackbarPos('LH','Tracking')
+    l_s = cv2.getTrackbarPos('LS','Tracking')
+    l_v = cv2.getTrackbarPos('LV','Tracking')
+    u_h = cv2.getTrackbarPos('UH','Tracking')
+    u_s = cv2.getTrackbarPos('US','Tracking')
+    u_v = cv2.getTrackbarPos('UV','Tracking')
+
+
+
+    l_b = np.array([l_h,l_s,l_v])
+    u_b = np.array([u_h,u_s,u_v])
+    mask = cv2.inRange(hsv,l_b,u_b)
+    res = cv2.bitwise_and(frame,frame,mask = mask)
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+
+    k = cv2.waitKey(1)
+    # press 'ESC' button to exit
+    if k ==27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
